bb5d1a757f142df9d1f6967aabedd30818509cb8
carloshnp <carloshnpwork@gmail.com>
2022-08-14 04:26:16 -0300

    commit_js_1

---
+++ script.js
@@ -0,0 +1,59 @@
+/*
+
+Estrutura do código:
+
+- Selecionar primeiro item
+- Selecionar segundo item
+- Selecionar terceiro item
+
+        # Ao selecionar os itens, apenas um item pode ficar selecionado (mutualmente exclusivo)
+        # O script deve ser capaz de armazenar o nome e o preço de cada item selecionado
+        # Logo, o script deve armazenar esses dados no momento da seleção dos itens para que eles já estejam processados na conclusão do pedido.
+
+- Ativar botão
+
+        # Para que o botão seja ativado, os três itens (um de cada seção) devem estar selecionados
+
+- No clique do botão:
+    - Ativar tela de confirmação do pedido
+    - Indicar itens selecionados e respectivos preços
+
+        # O script deve ser capaz de organizar os dados armazenados durante a seleção dos itens para aparecerem na tela escondida.
+
+    - Indicar o preço total do pedido
+
+        # O script deve ser capaz de realizar a soma dos preços dos itens
+
+    - Realizar ou cancelar pedido
+        - Ao cancelar, voltar para a tela principal de seleção
+
+            # O botão de cancelamento esconde a tela de confirmação, onde a troca dos itens pode ser realizada. Basicamente, o processamento do pedido (nomes + valores + total) acontece concomitantemente com a seleção dos itens do cardápio.
+
+        - Ao realizar pedido:
+            - Abrir prompt perguntando o nome e depois endereço
+            - Abrir WhatsApp e preparar mensagem de texto formatada
+
+*/
+
+let food_name = ''
+let drink_name = ''
+let dessert_name = ''
+let food_price = ''
+let drink_price = ''
+let dessert_price = ''
+let total_price = ''
+let name = ''
+let address = ''
+
+function select_item() {
+
+}
+
+function get_item_name() {
+
+}
+
+function get_item_price() {
+
+}
+
