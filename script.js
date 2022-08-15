c2209cc44dafd221591793af46a10ea627df6a60
carloshnp <carloshnpwork@gmail.com>
2022-08-15 05:47:57 -0300

    acho que acabou

--- script.js
+++ script.js
@@ -35,6 +35,8 @@ Estrutura do código: +1
 
 */
 
+// define variáveis que serão utilizadas de forma global
+
 let foodName;
 let drinkName;
 let dessertName;
@@ -42,19 +44,16 @@ let foodPrice;
 let drinkPrice;
 let dessertPrice;
 let totalPrice;
-let name;
-let address;
-let checkoutName;
 
 function selectFood(select) {
     // Essa função seleciona um item da seção Food, e coloca as variáveis foodName e foodPrice com o nome e o preço do item, respectivamente.
-    let selectedItem = document.querySelector('.food .selected'); // verifica se há um item já selecionado
+    let selectedItem = document.querySelector('.food .selected1'); // verifica se há um item já selecionado
     if (selectedItem !== null) {
-        selectedItem.classList.remove('selected');
+        selectedItem.classList.remove('selected1');
     } // remove a seleção do item já selecionado para selecionar outro item
-    select.classList.add('selected'); // seleciona o item (mudando ele)
-    foodName = document.querySelector('.selected .item-title').innerHTML; // pega o texto dentro do 'item-title' e define como o foodName
-    foodPrice = document.querySelector('.selected .item-price').innerHTML; // pega o texto dentro do 'item-price' e define como o foodPrice
+    select.classList.add('selected1'); // seleciona o item (mudando ele)
+    foodName = document.querySelector('.selected1 .item-title').innerHTML; // pega o texto dentro do 'item-title' e define como o foodName
+    foodPrice = document.querySelector('.selected1 .item-price').innerHTML; // pega o texto dentro do 'item-price' e define como o foodPrice
     foodPrice = foodPrice.replace('R$ ', ''); // deixa apenas o texto em número do preço do item
     checkoutName = document.querySelector('.food-name'); // seleciona a categoria do item no checkout
     checkoutName.innerHTML = foodName; // adiciona o nome do item no checkout
@@ -66,13 +65,13 @@ function selectFood(select) {
 
 function selectDrink(select) {
     // Essa função seleciona um item da seção Drink, e coloca as variáveis drinkName e drinkPrice com o nome e o preço do item, respectivamente.
-    let selectedItem = document.querySelector('.drink .selected');
+    let selectedItem = document.querySelector('.drink .selected2');
     if (selectedItem !== null) {
-        selectedItem.classList.remove('selected');
+        selectedItem.classList.remove('selected2');
     }
-    select.classList.add('selected');
-    drinkName = document.querySelector('.selected .item-title').innerHTML;
-    drinkPrice = document.querySelector('.selected .item-price').innerHTML;
+    select.classList.add('selected2');
+    drinkName = document.querySelector('.selected2 .item-title').innerHTML;
+    drinkPrice = document.querySelector('.selected2 .item-price').innerHTML;
     drinkPrice = drinkPrice.replace('R$ ', '');
     checkoutName = document.querySelector('.drink-name');
     checkoutName.innerHTML = drinkName;
@@ -85,13 +84,13 @@ function selectDrink(select) {
 
 function selectDessert(select) {
     // Essa função seleciona um item da seção Dessert, e coloca as variáveis dessertName e dessertPrice com o nome e o preço do item, respectivamente.
-    let selectedItem = document.querySelector('.dessert .selected'); 
+    let selectedItem = document.querySelector('.dessert .selected3'); 
     if (selectedItem !== null) {
-        selectedItem.classList.remove('selected');
+        selectedItem.classList.remove('selected3');
     }
-    select.classList.add('selected'); 
-    dessertName = document.querySelector('.selected .item-title').innerHTML; 
-    dessertPrice = document.querySelector('.selected .item-price').innerHTML;
+    select.classList.add('selected3'); 
+    dessertName = document.querySelector('.selected3 .item-title').innerHTML; 
+    dessertPrice = document.querySelector('.selected3 .item-price').innerHTML;
     dessertPrice = dessertPrice.replace('R$ ', '');
     checkoutName = document.querySelector('.dessert-name');
     checkoutName.innerHTML = dessertName;
@@ -111,17 +110,42 @@ function orderEnabled() {
 }
 
 function checkout() {
+    // revela o menu de checkout ao clicar no botão de order e calcula o preço total dos itens
     if (foodName && drinkName && dessertName !== undefined) {
         let unhide = document.querySelector('.checkout-container')
         unhide.classList.remove('hidden')
     }
     totalPriceCalc();
     let total = document.querySelector('.total-price');
-    total.innerHTML = totalPrice
+    total.innerHTML = 'R$ ' + totalPrice
 }
 
 function totalPriceCalc() {
+    // calcula o preço total dos itens
     totalPrice = Number(foodPrice.replace(',', '.')) + Number(drinkPrice.replace(',', '.')) + Number(dessertPrice.replace(',', '.'));
     totalPrice = totalPrice.toFixed(2);
     totalPrice = totalPrice.replace('.', ',')
+}
+
+function cancel() {
+    // cancela o checkout e volta para a tela de seleção
+    let hide = document.querySelector('.checkout-container');
+    hide.classList.add('hidden');
+}
+
+function orderComplete() {
+    // pede o nome e o endereço do usuário, e gera uma mensagem formatada com os dados do pedido para ser enviado para um número no WhatsApp
+    let name = prompt('Qual é o seu nome?');
+    let address = prompt('Qual é o seu endereço?');
+    let orderReceipt = `Olá, gostaria de fazer o pedido:
+- Prato: ${foodName}
+- Bebida: ${drinkName}
+- Sobremesa: ${dessertName}
+Total: R$ ${totalPrice}
+
+Nome: ${name}
+Endereço: ${address}`;
+    let orderFormatted = encodeURIComponent(orderReceipt)
+    let whatsapp = "https://wa.me/5521972408376?text=" + orderFormatted;
+    window.open(whatsapp, '_blank').focus();
 }
