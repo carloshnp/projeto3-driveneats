db6e38ddda71f90f9b68f27801800d16ce0b72d6
carloshnp <carloshnpwork@gmail.com>
2022-08-15 04:51:47 -0300

    quase lá

--- script.js
+++ script.js
@@ -35,58 +35,75 @@ Estrutura do código: +1
 
 */
 
-let food_name;
-let drink_name;
-let dessert_name;
-let food_price;
-let drink_price;
-let dessert_price;
-let total_price;
+let foodName;
+let drinkName;
+let dessertName;
+let foodPrice;
+let drinkPrice;
+let dessertPrice;
+let totalPrice;
 let name;
 let address;
+let checkoutName;
 
 function selectFood(select) {
-    // Essa função seleciona um item da seção Food, e coloca as variáveis food_name e food_price com o nome e o preço do item, respectivamente.
+    // Essa função seleciona um item da seção Food, e coloca as variáveis foodName e foodPrice com o nome e o preço do item, respectivamente.
     let selectedItem = document.querySelector('.food .selected'); // verifica se há um item já selecionado
     if (selectedItem !== null) {
         selectedItem.classList.remove('selected');
     } // remove a seleção do item já selecionado para selecionar outro item
     select.classList.add('selected'); // seleciona o item (mudando ele)
-    food_name = document.querySelector('.selected .item-title').innerHTML; // pega o texto dentro do 'item-title' e define como o food_name
-    food_price = document.querySelector('.selected .item-price').innerHTML; // pega o texto dentro do 'item-price' e define como o food_price
+    foodName = document.querySelector('.selected .item-title').innerHTML; // pega o texto dentro do 'item-title' e define como o foodName
+    foodPrice = document.querySelector('.selected .item-price').innerHTML; // pega o texto dentro do 'item-price' e define como o foodPrice
+    foodPrice = foodPrice.replace('R$ ', ''); // deixa apenas o texto em número do preço do item
+    checkoutName = document.querySelector('.food-name'); // seleciona a categoria do item no checkout
+    checkoutName.innerHTML = foodName; // adiciona o nome do item no checkout
+    checkoutPrice = document.querySelector('.food-price'); // seleciona o preço do item no checkout
+    checkoutPrice.innerHTML = foodPrice; // adiciona o preço do item no checkout
 
     orderEnabled(); // verifica se os itens foram selecionados para ativar o botão do checkout
 }
 
 function selectDrink(select) {
-    // Essa função seleciona um item da seção Drink, e coloca as variáveis drink_name e drink_price com o nome e o preço do item, respectivamente.
+    // Essa função seleciona um item da seção Drink, e coloca as variáveis drinkName e drinkPrice com o nome e o preço do item, respectivamente.
     let selectedItem = document.querySelector('.drink .selected');
     if (selectedItem !== null) {
         selectedItem.classList.remove('selected');
     }
     select.classList.add('selected');
-    drink_name = document.querySelector('.selected .item-title').innerHTML;
-    drink_price = document.querySelector('.selected .item-price').innerHTML;
+    drinkName = document.querySelector('.selected .item-title').innerHTML;
+    drinkPrice = document.querySelector('.selected .item-price').innerHTML;
+    drinkPrice = drinkPrice.replace('R$ ', '');
+    checkoutName = document.querySelector('.drink-name');
+    checkoutName.innerHTML = drinkName;
+    checkoutPrice = document.querySelector('.drink-price');
+    checkoutPrice.innerHTML = drinkPrice;
+
     
     orderEnabled();
 }
 
 function selectDessert(select) {
-    // Essa função seleciona um item da seção Dessert, e coloca as variáveis dessert_name e dessert_price com o nome e o preço do item, respectivamente.
+    // Essa função seleciona um item da seção Dessert, e coloca as variáveis dessertName e dessertPrice com o nome e o preço do item, respectivamente.
     let selectedItem = document.querySelector('.dessert .selected'); 
     if (selectedItem !== null) {
         selectedItem.classList.remove('selected');
     }
     select.classList.add('selected'); 
-    dessert_name = document.querySelector('.selected .item-title').innerHTML; 
-    dessert_price = document.querySelector('.selected .item-price').innerHTML;
+    dessertName = document.querySelector('.selected .item-title').innerHTML; 
+    dessertPrice = document.querySelector('.selected .item-price').innerHTML;
+    dessertPrice = dessertPrice.replace('R$ ', '');
+    checkoutName = document.querySelector('.dessert-name');
+    checkoutName.innerHTML = dessertName;
+    checkoutPrice = document.querySelector('.dessert-price');
+    checkoutPrice.innerHTML = dessertPrice;
 
     orderEnabled();
 }
 
 function orderEnabled() {
     // verifica se os itens foram selecionados para ativar o botão do checkout
-    if (food_name && drink_name && dessert_name !== undefined) {
+    if (foodName && drinkName && dessertName !== undefined) {
         let enabled = document.querySelector('.order-button');
         enabled.classList.add('enabled-order');
         enabled.innerHTML = 'Fechar pedido';
@@ -94,8 +111,17 @@ function orderEnabled() {
 }
 
 function checkout() {
-    let check = document.querySelector('.order-button .enabled-order');
-    if (check !== null) {
-        
+    if (foodName && drinkName && dessertName !== undefined) {
+        let unhide = document.querySelector('.checkout-container')
+        unhide.classList.remove('hidden')
     }
+    totalPriceCalc();
+    let total = document.querySelector('.total-price');
+    total.innerHTML = totalPrice
+}
+
+function totalPriceCalc() {
+    totalPrice = Number(foodPrice.replace(',', '.')) + Number(drinkPrice.replace(',', '.')) + Number(dessertPrice.replace(',', '.'));
+    totalPrice = totalPrice.toFixed(2);
+    totalPrice = totalPrice.replace('.', ',')
 }
